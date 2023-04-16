062d5fafcb6bce93d06a3da8f503bf54f3ef9b18
chen1weiting <118669941+chen1weiting@users.noreply.github.com>
2023-04-16 21:39:25 +0800

    Update hex_colours.py

--- prac-05/hex_colours.py
+++ prac-05/hex_colours.py
@@ -1,23 +1,23 @@
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-
-# Print all the states and names neatly lined up with string formatting
-for code, name in CODE_TO_NAME.items():
-    print("{:3s} is {}".format(code, name))
+COLORS = {
+    'ALICEBLUE': '#f0f8ff',
+    'ANTIQUEWHITE': '#faebd7',
+    'AQUA': '#00ffff',
+    'AZURE': '#f0ffff',
+    'BEIGE': '#f5f5dc',
+    'BISQUE': '#ffe4c4',
+    'BLACK': '#000000',
+    'BLANCHEDALMOND': '#ffebcd',
+    'BLUE': '#0000ff',
+    'BLUEVIOLET': '#8a2be2'
+}
 
 while True:
-    # Prompt for a state code and convert it to uppercase
-    state_code = input("Enter short state: ").upper()
-
-    # Check if the input is a valid state code using the EAFP approach
-    try:
-        # If the code is valid, print the state name
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    except KeyError:
-        # If the code is invalid, print an error message and prompt for another code
-        print("Invalid short state")
-        continue
-
-    # If the input is blank, stop the loop
-    if state_code == "":
+    color_name = input("Enter a color name (blank to stop): ").strip().upper()
+    if not color_name:
         break
+    
+    color_code = COLORS.get(color_name)
+    if color_code:
+        print(f"The hexadecimal code for {color_name} is {color_code}.")
+    else:
+        print(f"Sorry, we couldn't find a color code for {color_name}.")
