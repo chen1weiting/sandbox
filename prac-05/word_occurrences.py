0a8737efbba5ea03f7008ec84578c8ae1902e5d9
chen1weiting <118669941+chen1weiting@users.noreply.github.com>
2023-04-16 21:43:03 +0800

    Update word_occurrences.py

--- prac-05/word_occurrences.py
+++ prac-05/word_occurrences.py
@@ -1,11 +1,15 @@
+from typing import Dict, Any
+
+
 def count_words(text):
- words = text.split()
- counts = {}
- for word in words:
-   if word in counts:
-     counts[word] += 1
-   else:
-     counts[word] = 1
- return counts
+    words = text.split()
+    counts: dict[Any, int] = {}
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+
 
 print(count_words("text"))
