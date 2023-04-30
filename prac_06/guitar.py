6ffa988f7bdda5ddbbd3df62dfbdaeea6081761f
chen1weiting <118669941+chen1weiting@users.noreply.github.com>
2023-04-30 21:12:21 +0800

    Update guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,4 +1,14 @@
-name = "Gibson L-5 CES"
-year = 1922
-cost = 16035.40
-print(f"My guitar: {name}, first made in {year}")
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+    
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+    
+    def get_age(self):
+        return 2022 - self.year
+    
+    def is_vintage(self):
+        return self.get_age() >= 50
